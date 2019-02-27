2de57fa91892392275acbe70ecc80b9d9e278b8d
Albertas Dvirnas <retardedshrimp@gmail.com>
2019-02-27 09:52:36 +0100

    Add DBM script

---
+++ src/HCA_DBM.m
@@ -0,0 +1,17 @@
+function [hcaStruct] = HCA_DBM(sets, hcaStruct)
+    % HCA_DBM
+    % Used for extracting kymographs from movie structure (basically copies AB_Gui/DBM_Gui commands)
+    %
+    %     Args:
+    %         sets (struct): Input settings to the method
+    %         hcaStruct (struct): Input structure, if non-empty, load
+    %         result structure instead of computing everything
+    %
+    %     Returns:
+    %         hcaStruct: Return structure
+    %
+    %     Example:
+    %         This is an example: run [hcaStruct] = HCA_DBM(sets)
+    %
+
+end
