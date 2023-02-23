d19af6bb8676295d8dbff4ce1068551c60aa2d61
albertas albertas <albdvir@gmail.com>
2023-02-23 15:46:20 +0100

    small bug fix when some theories not generated

--- src/+CBT/+Hca/+Core/+Analysis/convert_nm_ratio.m
+++ src/+CBT/+Hca/+Core/+Analysis/convert_nm_ratio.m
@@ -16,75 +16,76 @@ function [ theoryStruct ] = convert_nm_ratio( newNmBp, theoryStruct,sets)
     precision = sets.theory.precision;
     matDirpath = sets.output.matDirpath;
     for i=1:length(theoryStruct)
-
-        % first change nm to bp ratio
-        fileID = fopen(theoryStruct{i}.filename,'r');
-        formatSpec = '%f';
-        seq = fscanf(fileID,formatSpec);
-        fclose(fileID);
-
-        % first convert to the correct length
-        pxSize = theoryStruct{i}.meanBpExt_nm/newNmBp;
-        
-        import CBT.Core.convert_bpRes_to_pxRes;
-        % convert nm ratio bitmask
-        try % only if bitmask exists. first try to load
-            bitname = strrep(theoryStruct{i}.filename,'barcode.txt','bitmask.txt');
-            fileID = fopen(bitname,'r');
-            bitmask = fscanf(fileID,' %f');
+        if theoryStruct{i}.length ~= 0
+    
+            % first change nm to bp ratio
+            fileID = fopen(theoryStruct{i}.filename,'r');
+            formatSpec = '%f';
+            seq = fscanf(fileID,formatSpec);
             fclose(fileID);
-            bitmask = convert_bpRes_to_pxRes(bitmask, 1/pxSize);
-            [~,mi,en] =fileparts(bitname);
-            bitname = fullfile(fullfile(matDirpath,'theories'),strcat([mi '_converted_to' num2str(newNmBp) en ]));
-            fileID = fopen(bitname,'w');
-            fprintf(fileID,' %5.5f', bitmask);
-            fclose(fileID);
-        end
-        
-        % this should be correct since the convolution of two Gaussians is
-        % a Gaussian, a test of this is in the script nmbpconvertion.m
-        import CBT.Core.convert_bpRes_to_pxRes;
-        seq = convert_bpRes_to_pxRes(seq, 1/pxSize);
-%         seq = interp1(1:length(seq),seq,linspace(1,length(seq),(length(seq)*1/pxSize)));
-        
-%         figure,plot(seq1)
-%         hold on
-%         plot(seq2)
-%         legend({'converted to bp','Interpolated'})
-%         
-%         % We assume bitmasks to be only ones for theories
-        %hcaSessionStruct.theoryGen.bitmask{i} = convert_bpRes_to_pxRes(hcaSessionStruct.theoryGen.bitmask{i}, 1/pxSize);
-        sigma1 = (1/pxSize)*theoryStruct{i}.psfSigmaWidth_nm/theoryStruct{i}.pixelWidth_nm;
-        sigma =  theoryStruct{i}.psfSigmaWidth_nm/theoryStruct{i}.pixelWidth_nm;
-        % size of final sigma
-        sigmaDif = sqrt(sigma^2-sigma1^2);
+    
+            % first convert to the correct length
+            pxSize = theoryStruct{i}.meanBpExt_nm/newNmBp;
+            
+            import CBT.Core.convert_bpRes_to_pxRes;
+            % convert nm ratio bitmask
+            try % only if bitmask exists. first try to load
+                bitname = strrep(theoryStruct{i}.filename,'barcode.txt','bitmask.txt');
+                fileID = fopen(bitname,'r');
+                bitmask = fscanf(fileID,' %f');
+                fclose(fileID);
+                bitmask = convert_bpRes_to_pxRes(bitmask, 1/pxSize);
+                [~,mi,en] =fileparts(bitname);
+                bitname = fullfile(fullfile(matDirpath,'theories'),strcat([mi '_converted_to' num2str(newNmBp) en ]));
+                fileID = fopen(bitname,'w');
+                fprintf(fileID,' %5.5f', bitmask);
+                fclose(fileID);
+            end
+            
+            % this should be correct since the convolution of two Gaussians is
+            % a Gaussian, a test of this is in the script nmbpconvertion.m
+            import CBT.Core.convert_bpRes_to_pxRes;
+            seq = convert_bpRes_to_pxRes(seq, 1/pxSize);
+    %         seq = interp1(1:length(seq),seq,linspace(1,length(seq),(length(seq)*1/pxSize)));
+            
+    %         figure,plot(seq1)
+    %         hold on
+    %         plot(seq2)
+    %         legend({'converted to bp','Interpolated'})
+    %         
+    %         % We assume bitmasks to be only ones for theories
+            %hcaSessionStruct.theoryGen.bitmask{i} = convert_bpRes_to_pxRes(hcaSessionStruct.theoryGen.bitmask{i}, 1/pxSize);
+            sigma1 = (1/pxSize)*theoryStruct{i}.psfSigmaWidth_nm/theoryStruct{i}.pixelWidth_nm;
+            sigma =  theoryStruct{i}.psfSigmaWidth_nm/theoryStruct{i}.pixelWidth_nm;
+            % size of final sigma
+            sigmaDif = sqrt(sigma^2-sigma1^2);
+           
+            % length of kernel
+            hsize = size(seq,2);
        
-        % length of kernel
-        hsize = size(seq,2);
-   
-        % kernel
-        ker = circshift(images.internal.createGaussianKernel(sigmaDif, hsize),round(hsize/2));   
-        
-        % conjugate of kernel in phase space
-        multF=conj(fft(ker'));
-
-        % convolved with sequence ->
-        seq = ifft(fft(seq).*multF); 
-        %fname = strcat(['fold/theory_' barcodeData.hcaSessionStruct.theoryNames{i} '.txt']);
-        [~,mi,en] =fileparts(theoryStruct{i}.filename);
-        
-        try        mkdir(fullfile(matDirpath,'theories'));
-        catch
+            % kernel
+            ker = circshift(images.internal.createGaussianKernel(sigmaDif, hsize),round(hsize/2));   
+            
+            % conjugate of kernel in phase space
+            multF=conj(fft(ker'));
+    
+            % convolved with sequence ->
+            seq = ifft(fft(seq).*multF); 
+            %fname = strcat(['fold/theory_' barcodeData.hcaSessionStruct.theoryNames{i} '.txt']);
+            [~,mi,en] =fileparts(theoryStruct{i}.filename);
+            
+            try        mkdir(fullfile(matDirpath,'theories'));
+            catch
+            end
+            theoryStruct{i}.filename = fullfile(fullfile(matDirpath,'theories'),strcat([mi '_converted_to' num2str(newNmBp) en ]));
+    
+            fileID = fopen(theoryStruct{i}.filename,'w');
+            fprintf(fileID,strcat([' %2.' num2str(precision) 'f ']), seq);
+            fclose(fileID);
+            theoryStruct{i}.meanBpExt_nm = newNmBp;
+            theoryStruct{i}.length = length(seq);
+            
         end
-        theoryStruct{i}.filename = fullfile(fullfile(matDirpath,'theories'),strcat([mi '_converted_to' num2str(newNmBp) en ]));
-
-        fileID = fopen(theoryStruct{i}.filename,'w');
-        fprintf(fileID,strcat([' %2.' num2str(precision) 'f ']), seq);
-        fclose(fileID);
-        theoryStruct{i}.meanBpExt_nm = newNmBp;
-        theoryStruct{i}.length = length(seq);
-        
-
     end
 end
 
